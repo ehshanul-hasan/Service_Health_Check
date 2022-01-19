111e7574108f207c81d7ecf5cebf07ec1f1c9311
ehshanul.hasan <ehshanul.hasan@BDDHA2WLT02290>
2022-01-19 17:41:18 +0600

    Add project files.

---
+++ MicroServiceOne/Program.cs
@@ -0,0 +1,37 @@
+using HealthChecks.UI.Client;
+using MicroServiceOne.Repository;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationContext>();
+//Add Health Check
+builder.Services.AddHealthChecks()
+    .AddDbContextCheck<ApplicationContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
---
+++ MicroServiceOne/Repository/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroServiceOne.Repository
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Application_Src");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Add Dummy Data Here If Needed
+            //base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
