e1d2f7d645562597646ae1f87ee7fd5339f831bb
Ehshanul Hasan <ehtousif@outlook.com>
2022-01-19 17:50:27 +0600

    health check comment added

--- MicroServiceOne/Program.cs
+++ MicroServiceOne/Program.cs
@@ -28,6 +28,8 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+// health check endpoint config
 app.MapHealthChecks("/hc", new HealthCheckOptions()
 {
     Predicate = _ => true,
